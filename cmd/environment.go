bd771974a99bce90dbcc9c95202723ba4ebe9286
Ben Jackson <b@benjackson.email>
2019-11-05 22:27:46 +1100

    refactor flags for environment delete

--- cmd/environment.go
+++ cmd/environment.go
@@ -1,43 +1,49 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/amazeeio/lagoon-cli/lagoon/environments"
 	"github.com/amazeeio/lagoon-cli/output"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
+
+// EnvironmentFlags .
+type EnvironmentFlags struct {
+	Project     string `json:"project,omitempty"`
+	Environment string `json:"environment,omitempty"`
+}
+
+func parseEnvironmentFlags(flags pflag.FlagSet) EnvironmentFlags {
+	configMap := make(map[string]interface{})
+	flags.VisitAll(func(f *pflag.Flag) {
+		if flags.Changed(f.Name) {
+			configMap[f.Name] = f.Value
+		}
+	})
+	jsonStr, _ := json.Marshal(configMap)
+	parsedFlags := EnvironmentFlags{}
+	json.Unmarshal(jsonStr, &parsedFlags)
+	return parsedFlags
+}
 
 var deleteEnvCmd = &cobra.Command{
 	Use:   "environment [project name] [environment name]",
 	Short: "Delete an environment",
 	Run: func(cmd *cobra.Command, args []string) {
-		var projectName string
-		var projectEnvironment string
-		if len(args) != 0 || cmdProject.Name == "" {
-			if len(args) < 2 {
-				fmt.Println("Not enough arguments. Requires: project name and environment")
-				cmd.Help()
-				os.Exit(1)
-			}
-			projectName = args[0]
-			projectEnvironment = args[1]
-		} else {
-			if len(args) < 1 {
-				fmt.Println("Not enough arguments. Requires: environment")
-				cmd.Help()
-				os.Exit(1)
-			}
-			projectName = strings.TrimSpace(cmdProject.Name)
-			projectEnvironment = args[0]
+		environmentFlags := parseEnvironmentFlags(*cmd.Flags())
+		if environmentFlags.Project == "" && environmentFlags.Environment == "" {
+			fmt.Println("Not enough arguments. Requires: project name and environment name")
+			cmd.Help()
+			os.Exit(1)
 		}
-
-		fmt.Println(fmt.Sprintf("Deleting %s-%s", projectName, projectEnvironment))
+		fmt.Println(fmt.Sprintf("Deleting %s-%s", environmentFlags.Project, environmentFlags.Environment))
 
 		if yesNo() {
-			projectByName, err := environments.DeleteEnvironment(projectName, projectEnvironment)
+			projectByName, err := environments.DeleteEnvironment(environmentFlags.Project, environmentFlags.Environment)
 			if err != nil {
 				output.RenderError(err.Error(), outputOptions)
 				os.Exit(1)
@@ -47,7 +53,6 @@ var deleteEnvCmd = &cobra.Command{
 			}
 			output.RenderResult(resultData, outputOptions)
 		}
-
 	},
 }
 
