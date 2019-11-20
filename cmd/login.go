740853d991b966c572a95973c66527ad679ad2cb
Ben Jackson <b@benjackson.email>
2019-11-20 12:03:50 +1100

    being able to use an encrypted ssh key and ssh agent would be nice

--- cmd/login.go
+++ cmd/login.go
@@ -3,12 +3,15 @@ package cmd
 import (
 	"fmt"
 	"io/ioutil"
+	"net"
 	"os"
 	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 var loginCmd = &cobra.Command{
@@ -25,32 +28,63 @@ var loginCmd = &cobra.Command{
 	},
 }
 
-func publicKey(path string) ssh.AuthMethod {
+func publicKey(path string) (ssh.AuthMethod, func() error) {
+	noopCloseFunc := func() error { return nil }
+
 	key, err := ioutil.ReadFile(path)
 	if err != nil {
 		panic(err)
 	}
+
+	// Connect to SSH agent to ask for unencrypted private keys
+	if sshAgentConn, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err == nil {
+		sshAgent := agent.NewClient(sshAgentConn)
+
+		keys, _ := sshAgent.List()
+		if len(keys) > 0 {
+			// There are key(s) in the agent
+			//defer sshAgentConn.Close()
+			return ssh.PublicKeysCallback(sshAgent.Signers), sshAgentConn.Close
+		}
+		fmt.Println("no agent")
+	}
+
+	// Try to look for an unencrypted private key
 	signer, err := ssh.ParsePrivateKey(key)
 	if err != nil {
-		panic(err)
+		fmt.Println("Error was:", err.Error())
+		os.Exit(1)
+	} else if err == nil {
+		// return unencrypted private key
+		return ssh.PublicKeys(signer), noopCloseFunc
 	}
-	return ssh.PublicKeys(signer)
+
+	// Handle encrypted private keys
+	fmt.Println("Found an encrypted private key!")
+	fmt.Printf("Enter passphrase for '%s': ", path)
+	bytePassword, err := terminal.ReadPassword(int(os.Stdin.Fd()))
+	fmt.Println()
+	signer, err = ssh.ParsePrivateKeyWithPassphrase(key, bytePassword)
+	return ssh.PublicKeys(signer), noopCloseFunc
 }
 
 func loginToken() error {
 	homeDir, _ := os.UserHomeDir()
+
 	privateKey := fmt.Sprintf("%s/.ssh/id_rsa", homeDir)
 	if cmdSSHKey != "" {
 		privateKey = cmdSSHKey
-		fmt.Println(privateKey)
 	}
+
+	authMethod, closeSSHAgent := publicKey(privateKey)
 	config := &ssh.ClientConfig{
 		User: "lagoon",
 		Auth: []ssh.AuthMethod{
-			publicKey(privateKey),
+			authMethod,
 		},
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
+	defer closeSSHAgent()
 	var err error
 
 	conn, err := ssh.Dial("tcp", fmt.Sprintf("%s:%s", viper.GetString("lagoons."+cmdLagoon+".hostname"), viper.GetString("lagoons."+cmdLagoon+".port")), config)
