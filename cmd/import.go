60249157ad2fc515b183478c7102556feea55ea5
Ben Jackson <b@benjackson.email>
2019-12-04 17:41:52 +1100

    import can also add notifications to projects

--- cmd/import.go
+++ cmd/import.go
@@ -9,48 +9,81 @@ import (
 	"github.com/spf13/viper"
 )
 
-var importCmd = &cobra.Command{
-	Use:     "import",
-	Aliases: []string{"i"},
-	Short:   "Import a config from a yaml file",
-	Long: `Import a YAML file, see following example:
-groups:
+var showExample bool
+var example = `groups:
   - name: example-com
 users:
   - user:
       email: usera@example.com
-      sshkey: ~/usera.pub
+      sshkey: ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQC+qXkqFFmZiQzEE9yhwEbHSuge187PRbH+MhEo3xyKpSODSa5Arp3P/ZPjxOOPm/KU2m9S/k4MKwvtZehqQtTDss9KaDZcZmwdYQSyJDgUzcAcMtEAFO8+9bVDIzgRXsRwsgkaJM627BRrutjpaaAryMMQJyuF/QMDwAfZ6ZBKNw== usera@macbook.local
     groups:
       - name: example-com
         role: owner
   - user:
       email: userb@example.com
-      sshkey: ~/userb.pub
+      sshkey: ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQC19Ap4udFpuIW7OGP3E0xSMr30c56uzp26thVmonvPWunU6pwcdV/hGioShSSUEWZdUwPhhCkUG4c/IrD0g6W6ZG/8JoLkUkIQ0OMSRl6zDGy0woTrbURyktwHLfzV12WLFmJR6l7egHov/FgNVx4gRRvVLIEwfpbGHvNCG6lQnQ== userb@macbook.local
     groups:
       - name: example-com
-        role: developere
+        role: developer
+slack:
+  - name: example-com-slack
+    webhook: https://slack.com/webhook
+    channel: example-com
+rocketchat:
+  - name: example-com-rocketchat
+    webhook: https://rocketchat.com/webhook
+    channel: example-com
+  - name: example-com-api-rocketchat
+    webhook: https://rocketchat.com/webhook
+    channel: example-com-api
 projects:
-  - name: example-com
-    giturl: git@github.com:example/example-com.git
-    openshift: 1
-    branches: "master|develop|staging"
-    productionenvironment: master
+  - project:
+      name: example-com
+      giturl: "git@github.com:example/example-com.git"
+      openshift: 1
+      branches: master|develop|staging
+      productionenvironment: master
+    notifications:
+      slack:
+        - example-com-slack
+      rocketchat:
+        - example-com-rocketchat
     groups:
       - example-com
-`,
+  - project:
+      name: example-com-api
+      giturl: "git@github.com:example/example-com-api.git"
+      openshift: 1
+      branches: master|develop|staging
+      productionenvironment: master
+    notifications:
+      rocketchat:
+        - example-com-api-rocketchat
+    groups:
+      - example-com`
+
+var importCmd = &cobra.Command{
+	Use:     "import",
+	Aliases: []string{"i"},
+	Short:   "Import a config from a yaml file",
 	Run: func(cmd *cobra.Command, args []string) {
 		validateToken(viper.GetString("current")) // get a new token if the current one is invalid
+		if showExample {
+			fmt.Println(example)
+			os.Exit(0)
+		}
 		if importFile == "" {
 			fmt.Println("Not enough arguments. Requires: path to file to import")
 			cmd.Help()
 			os.Exit(1)
 		}
 		if yesNo("Are you sure you want to import this file, it is potentially dangerous") {
-			importer.ImportData(importFile)
+			importer.ImportData(importFile, forceAction)
 		}
 	},
 }
 
 func init() {
+	importCmd.Flags().BoolVarP(&showExample, "example", "", false, "display example yaml")
 	importCmd.Flags().StringVarP(&importFile, "import", "I", "", "path to the file to import")
 }
--- lagoon/importer/main.go
+++ lagoon/importer/main.go
@@ -10,16 +10,38 @@ import (
 	"github.com/amazeeio/lagoon-cli/api"
 	"github.com/amazeeio/lagoon-cli/lagoon/projects"
 	"github.com/amazeeio/lagoon-cli/lagoon/users"
+	"github.com/manifoldco/promptui"
 	"gopkg.in/yaml.v2"
 )
 
 type lagoonImport struct {
-	Groups []api.Group `json:"groups"`
-	Users  []struct {
-		User   api.User         `json:"user"`
+	Groups     []api.Group                  `json:"groups"`
+	Slack      []api.NotificationSlack      `json:"slack"`
+	RocketChat []api.NotificationRocketChat `json:"rocketchat"`
+	Users      []struct {
+		User struct {
+			Email      string `json:"email"`
+			SSHKey     string `json:"sshkey"`
+			KeyName    string `json:"keyname,omitempty"`
+			SSHKeyFile string `json:"sshkeyfile,omitempty"`
+		} `json:"user"`
 		Groups []addUserToGroup `json:"groups"`
 	} `json:"users"`
-	Projects []api.ProjectPatch `json:"projects"`
+	Projects []struct {
+		Project api.ProjectPatch `json:"project"`
+		// Project struct {
+		// 	Name                  string `yaml:"name"`
+		// 	GitURL                string `yaml:"gitUrl"`
+		// 	Openshift             int    `yaml:"openshift"`
+		// 	Branches              string `yaml:"branches"`
+		// 	ProductionEnvironment string `yaml:"productionEnvironment"`
+		// } `json:"project"`
+		Groups        []string `json:"groups"`
+		Notifications struct {
+			Slack      []string `json:"slack"`
+			RocketChat []string `json:"rocketchat"`
+		} `json:"notifications"`
+	} `json:"projects"`
 }
 
 type addUserToGroup struct {
@@ -56,13 +78,14 @@ type addUserToGroup struct {
 // `
 
 // ImportData func
-func ImportData(importFile string) {
+func ImportData(importFile string, forceAction bool) {
 	yamlData, err := ioutil.ReadFile(importFile) // just pass the file name
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	importedData := lagoonImport{}
+
 	err = yaml.Unmarshal([]byte(yamlData), &importedData)
 	if err != nil {
 		fmt.Println(err)
@@ -70,24 +93,60 @@ func ImportData(importFile string) {
 	}
 	// @TODO: do a platform-owner only check and fail sooner, tell user import is only for platform-owners currently
 	for _, group := range importedData.Groups {
-		fmt.Println(group.Name)
-		customReqResult, err := users.AddGroup(group)
+		fmt.Println("Adding group", group.Name)
+		_, err := users.AddGroup(group)
 		if err != nil {
 			fmt.Println(err)
-			os.Exit(1)
+			if !yesNo("Continue?", forceAction) {
+				os.Exit(1)
+			}
 		}
-		fmt.Println(string(customReqResult))
+		// fmt.Println(string(customReqResult))
 	}
 	for _, user := range importedData.Users {
-		fmt.Println(user.User.Email)
-		customReqResult, err := users.AddUser(user.User)
+		fmt.Println("Adding user", user.User.Email)
+		addUser := api.User{
+			Email: user.User.Email,
+		}
+		_, err := users.AddUser(addUser)
 		if err != nil {
 			fmt.Println(err)
-			os.Exit(1)
+			if !yesNo("Continue?", forceAction) {
+				os.Exit(1)
+			}
 		}
-		fmt.Println(string(customReqResult))
+		splitKey := strings.Split(user.User.SSHKey, " ")
+		var keyType api.SSHKeyType
+		// default to ssh-rsa, otherwise check if ssh-ed25519
+		// will fail if neither are right
+		keyType = api.SSHRsa
+		if strings.EqualFold(string(splitKey[0]), "ssh-ed25519") {
+			keyType = api.SSHEd25519
+		}
+		// if the sshkey has a comment/name in it, we can use that, otherwise define one using `keyname`
+		keyName := user.User.KeyName
+		if keyName == "" && len(splitKey) == 3 {
+			//strip new line
+			keyName = strings.TrimSuffix(splitKey[2], "\n")
+		} else if keyName == "" && len(splitKey) == 2 {
+			fmt.Println(err)
+			if !yesNo("Continue?", forceAction) {
+				os.Exit(1)
+			}
+		}
+		sshKey := api.SSHKey{
+			KeyType:  keyType,
+			KeyValue: splitKey[1],
+			Name:     keyName,
+		}
+		_, err = users.AddSSHKeyToUser(addUser, sshKey)
+		if err != nil {
+			fmt.Println(err)
+			if !yesNo("Continue?", forceAction) {
+				os.Exit(1)
+			}
+		}
 		for _, group := range user.Groups {
-			fmt.Println(group.Role)
 			var roleType api.GroupRole
 			roleType = api.GuestRole
 			if strings.EqualFold(string(group.Role), "guest") {
@@ -102,43 +161,121 @@ func ImportData(importFile string) {
 				roleType = api.OwnerRole
 			}
 			userGroupRole := api.UserGroupRole{
-				User: user.User,
+				User: addUser,
 				Group: api.Group{
 					Name: group.Name,
 				},
 				Role: roleType,
 			}
-			var customReqResult []byte
 			var err error
-			customReqResult, err = users.AddUserToGroup(userGroupRole)
+			fmt.Println("Adding user", user.User.Email, "to group", group.Name)
+			_, err = users.AddUserToGroup(userGroupRole)
 			if err != nil {
 				fmt.Println(err)
-				os.Exit(1)
+				if !yesNo("Continue?", forceAction) {
+					os.Exit(1)
+				}
 			}
-			returnResultData := map[string]interface{}{}
-			err = json.Unmarshal([]byte(customReqResult), &returnResultData)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-			fmt.Println(returnResultData)
 		}
 	}
-	for _, v := range importedData.Projects {
-		fmt.Println(v.Name, v.GitURL, v.ProductionEnvironment, v.Branches)
-		jsonPatch, _ := json.Marshal(v)
-		addResult, err := projects.AddProject(v.Name, string(jsonPatch))
+	for _, slack := range importedData.Slack {
+		fmt.Println("Adding slack", slack.Name)
+		_, err := projects.AddSlackNotification(slack.Name, slack.Channel, slack.Webhook)
 		if err != nil {
 			fmt.Println(err)
-			os.Exit(1)
+			if !yesNo("Continue?", forceAction) {
+				os.Exit(1)
+			}
 		}
-		var addedProject api.Project
-		err = json.Unmarshal([]byte(addResult), &addedProject)
+		// fmt.Println(string(customReqResult))
+	}
+	for _, rocketchat := range importedData.RocketChat {
+		fmt.Println("Adding rocketchat", rocketchat.Name)
+		_, err := projects.AddRocketChatNotification(rocketchat.Name, rocketchat.Channel, rocketchat.Webhook)
+		if err != nil {
+			fmt.Println(err)
+			if !yesNo("Continue?", forceAction) {
+				os.Exit(1)
+			}
+		}
+		// fmt.Println(string(customReqResult))
+	}
+	for _, project := range importedData.Projects {
+		jsonPatch, _ := json.Marshal(project.Project)
+		fmt.Println("Adding project", project.Project.Name)
+		addResult, err := projects.AddProject(project.Project.Name, string(jsonPatch))
+		if err != nil {
+			fmt.Println(err)
+			if !yesNo("Continue?", forceAction) {
+				os.Exit(1)
+			}
+			//os.Exit(1)
+		} else {
+			var addedProject api.Project
+			err = json.Unmarshal([]byte(addResult), &addedProject)
+			if err != nil {
+				fmt.Println(err)
+				if !yesNo("Continue?", forceAction) {
+					os.Exit(1)
+				}
+				//os.Exit(1)
+			}
+			// fmt.Println(addedProject)
+		}
+		for _, group := range project.Groups {
+			fmt.Println("Adding project", project.Project.Name, "to group", group)
+			projectGroup := api.ProjectGroups{
+				Project: api.Project{
+					Name: project.Project.Name,
+				},
+				Groups: []api.Group{
+					api.Group{
+						Name: group,
+					},
+				},
+			}
+			_, err = users.AddProjectToGroup(projectGroup)
+			if err != nil {
+				fmt.Println(err)
+				if !yesNo("Continue?", forceAction) {
+					os.Exit(1)
+				}
+			}
+		}
+		for _, slack := range project.Notifications.Slack {
+			fmt.Println("Adding slack", slack, "to project", project.Project.Name)
+			_, err = projects.AddSlackNotificationToProject(project.Project.Name, slack)
+			if err != nil {
+				fmt.Println(err)
+				if !yesNo("Continue?", forceAction) {
+					os.Exit(1)
+				}
+			}
+		}
+		for _, rocketchat := range project.Notifications.RocketChat {
+			fmt.Println("Adding rocketchat", rocketchat, "to project", project.Project.Name)
+			_, err = projects.AddRocketChatNotificationToProject(project.Project.Name, rocketchat)
+			if err != nil {
+				fmt.Println(err)
+				if !yesNo("Continue?", forceAction) {
+					os.Exit(1)
+				}
+			}
+		}
+	}
+}
 
+func yesNo(message string, forceAction bool) bool {
+	if forceAction != true {
+		prompt := promptui.Select{
+			Label: message + "; Select[Yes/No]",
+			Items: []string{"No", "Yes"},
+		}
+		_, result, err := prompt.Run()
 		if err != nil {
-			fmt.Println(err)
 			os.Exit(1)
 		}
-		fmt.Println(addedProject)
+		return result == "Yes"
 	}
+	return true
 }
