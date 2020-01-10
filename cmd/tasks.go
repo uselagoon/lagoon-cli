de74ada81dba1f4cb6aeeb4b2355940098d46cac
Ben Jackson <b@benjackson.email>
2020-01-10 16:34:04 +1100

    read from stdin, or defined path

--- cmd/tasks.go
+++ cmd/tasks.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/amazeeio/lagoon-cli/api"
@@ -103,6 +105,28 @@ var runCustomTask = &cobra.Command{
 	Aliases: []string{"c"},
 	Short:   "Run a custom command on an environment",
 	Run: func(cmd *cobra.Command, args []string) {
+		stat, _ := os.Stdin.Stat()
+		if (stat.Mode() & os.ModeCharDevice) == 0 {
+			// check if we are getting data froms stdin
+			scanner := bufio.NewScanner(os.Stdin)
+			taskCommand = ""
+			for scanner.Scan() {
+				taskCommand = taskCommand + scanner.Text() + "\n"
+			}
+			if err := scanner.Err(); err != nil {
+				fmt.Fprintln(os.Stderr, "reading standard input:", err)
+			}
+		} else {
+			// otherwise we can read from a file
+			if taskCommandFile != "" {
+				taskCommandBytes, err := ioutil.ReadFile(taskCommandFile) // just pass the file name
+				if err != nil {
+					fmt.Print(err)
+				}
+				taskCommand = string(taskCommandBytes)
+			}
+		}
+
 		if cmdProjectName == "" || cmdProjectEnvironment == "" || taskCommand == "" {
 			fmt.Println("Not enough arguments. Requires: project name, environment name, and command")
 			cmd.Help()
@@ -133,13 +157,15 @@ var runCustomTask = &cobra.Command{
 }
 
 var (
-	taskName    string
-	taskService string
-	taskCommand string
+	taskName        string
+	taskService     string
+	taskCommand     string
+	taskCommandFile string
 )
 
 func init() {
 	runCustomTask.Flags().StringVarP(&taskName, "name", "N", "Custom Task", "Name of the task that will show in the UI (default: Custom Task)")
 	runCustomTask.Flags().StringVarP(&taskService, "service", "S", "cli", "Name of the service (cli, nginx, other) that should run the task (default: cli)")
 	runCustomTask.Flags().StringVarP(&taskCommand, "command", "c", "", "The command to run in the task")
+	runCustomTask.Flags().StringVarP(&taskCommandFile, "script", "s", "", "Path to bash script to run (will use this before command(-c) if both are defined)")
 }
