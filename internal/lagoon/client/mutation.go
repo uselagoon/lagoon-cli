febaaec1b125129c749e7b00b42ac6700404f896
Scott Leggett <scott@sl.id.au>
2020-02-25 09:56:43 +0800

    Implement lagoon client
    Client implements Importer.

---
+++ internal/lagoon/client/mutation.go
@@ -0,0 +1,162 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+
+	"github.com/amazeeio/lagoon-cli/internal/lagoon"
+	"github.com/amazeeio/lagoon-cli/internal/schema"
+)
+
+var duplicate = regexp.MustCompile("^graphql: Duplicate entry ")
+
+// wrapErr wraps a response error with a lagoon.ErrExist type if the response
+// is due to an object already existing
+func wrapErr(err error) error {
+	if err != nil && duplicate.MatchString(err.Error()) {
+		return fmt.Errorf("couldn't create object: %w: %v", lagoon.ErrExist, err)
+	}
+	return err
+}
+
+// AddGroup adds a group.
+func (c *Client) AddGroup(
+	ctx context.Context, in *schema.AddGroupInput, out *schema.Group) error {
+	req, err := c.newRequest("_lgraphql/addGroup.graphql", in)
+	if err != nil {
+		return err
+	}
+	return c.client.Run(ctx, req, &struct {
+		Response *schema.Group `json:"addGroup"`
+	}{
+		Response: out,
+	})
+}
+
+// AddUser adds a user.
+func (c *Client) AddUser(
+	ctx context.Context, in *schema.AddUserInput, out *schema.User) error {
+	req, err := c.newRequest("_lgraphql/addUser.graphql", in)
+	if err != nil {
+		return err
+	}
+	return c.client.Run(ctx, req, &struct {
+		Response *schema.User `json:"addUser"`
+	}{
+		Response: out,
+	})
+}
+
+// AddSSHKey adds an SSH key to a user.
+func (c *Client) AddSSHKey(
+	ctx context.Context, in *schema.AddSSHKeyInput, out *schema.SSHKey) error {
+	req, err := c.newRequest("_lgraphql/addSshKey.graphql", in)
+	if err != nil {
+		return err
+	}
+	return c.client.Run(ctx, req, &struct {
+		Response *schema.SSHKey `json:"addSshKey"`
+	}{
+		Response: out,
+	})
+}
+
+// AddUserToGroup adds a user to a group.
+func (c *Client) AddUserToGroup(
+	ctx context.Context, in *schema.UserGroupRoleInput, out *schema.Group) error {
+	req, err := c.newRequest("_lgraphql/addUserToGroup.graphql", in)
+	if err != nil {
+		return err
+	}
+	return c.client.Run(ctx, req, &struct {
+		Response *schema.Group `json:"addUserToGroup"`
+	}{
+		Response: out,
+	})
+}
+
+// AddNotificationSlack defines a Slack notification.
+func (c *Client) AddNotificationSlack(ctx context.Context,
+	in *schema.AddNotificationSlackInput, out *schema.NotificationSlack) error {
+	req, err := c.newRequest("_lgraphql/addNotificationSlack.graphql", in)
+	if err != nil {
+		return err
+	}
+	return c.client.Run(ctx, req, &struct {
+		Response *schema.NotificationSlack `json:"addNotificationSlack"`
+	}{
+		Response: out,
+	})
+}
+
+// AddNotificationRocketChat defines a RocketChat notification.
+func (c *Client) AddNotificationRocketChat(ctx context.Context,
+	in *schema.AddNotificationRocketChatInput,
+	out *schema.NotificationRocketChat) error {
+	req, err := c.newRequest("_lgraphql/addNotificationRocketChat.graphql", in)
+	if err != nil {
+		return err
+	}
+	return c.client.Run(ctx, req, &struct {
+		Response *schema.NotificationRocketChat `json:"addNotificationRocketChat"`
+	}{
+		Response: out,
+	})
+}
+
+// AddProject adds a project.
+func (c *Client) AddProject(
+	ctx context.Context, in *schema.AddProjectInput, out *schema.Project) error {
+	req, err := c.newRequest("_lgraphql/addProject.graphql", in)
+	if err != nil {
+		return err
+	}
+	return wrapErr(c.client.Run(ctx, req, &struct {
+		Response *schema.Project `json:"addProject"`
+	}{
+		Response: out,
+	}))
+}
+
+// AddEnvVariable adds an EnvVariable to an Environment or Project.
+func (c *Client) AddEnvVariable(ctx context.Context,
+	in *schema.EnvVariableInput, out *schema.EnvKeyValue) error {
+	req, err := c.newRequest("_lgraphql/addEnvVariable.graphql", in)
+	if err != nil {
+		return err
+	}
+	return c.client.Run(ctx, req, &struct {
+		Response *schema.EnvKeyValue `json:"addEnvVariable"`
+	}{
+		Response: out,
+	})
+}
+
+// AddOrUpdateEnvironment adds or updates a Project Environment.
+func (c *Client) AddOrUpdateEnvironment(ctx context.Context,
+	in *schema.AddEnvironmentInput, out *schema.Environment) error {
+	req, err := c.newRequest("_lgraphql/addOrUpdateEnvironment.graphql", in)
+	if err != nil {
+		return err
+	}
+	return wrapErr(c.client.Run(ctx, req, &struct {
+		Response *schema.Environment `json:"addOrUpdateEnvironment"`
+	}{
+		Response: out,
+	}))
+}
+
+// AddGroupsToProject adds Groups to a Project.
+func (c *Client) AddGroupsToProject(ctx context.Context,
+	in *schema.ProjectGroupsInput, out *schema.Project) error {
+	req, err := c.newRequest("_lgraphql/addGroupsToProject.graphql", in)
+	if err != nil {
+		return err
+	}
+	return c.client.Run(ctx, req, &struct {
+		Response *schema.Project `json:"addGroupsToProject"`
+	}{
+		Response: out,
+	})
+}
