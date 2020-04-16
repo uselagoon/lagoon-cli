78482d089cad081482b52cd29afd0d89281a25bd
Steve Worley <sj.worley88@gmail.com>
2020-04-16 22:08:26 +1000

    Remove delete alias.
    - Removes the delete alias to make it more deliberate.
    
    Fixes #121.

--- cmd/delete.go
+++ cmd/delete.go
@@ -6,9 +6,8 @@ import (
 )
 
 var deleteCmd = &cobra.Command{
-	Use:     "delete",
-	Aliases: []string{"d"},
-	Short:   "Delete a project, or delete notifications and variables from projects or environments",
+	Use:   "delete",
+	Short: "Delete a project, or delete notifications and variables from projects or environments",
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		validateToken(viper.GetString("current")) // get a new token if the current one is invalid
 	},
