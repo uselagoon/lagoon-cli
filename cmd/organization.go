ccb628f17814c9c4bd2a98be5436981a58326cdc
cgoodwin90 <chris_2190@hotmail.com>
2023-09-27 12:13:42 +1000

    Add Org cmd

--- cmd/organization.go
+++ cmd/organization.go
@@ -1,3 +1,64 @@
 package cmd
 
-//TODO
+import (
+	"context"
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/uselagoon/lagoon-cli/pkg/output"
+	lclient "github.com/uselagoon/machinery/api/lagoon/client"
+	s "github.com/uselagoon/machinery/api/schema"
+	"os"
+)
+
+// TODO
+var addOrganizationCmd = &cobra.Command{
+	Use:     "organization",
+	Aliases: []string{"o"},
+	Short:   "Add a new organization to Lagoon",
+	PreRunE: func(_ *cobra.Command, _ []string) error {
+		return validateTokenE(lagoonCLIConfig.Current)
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		debug, err := cmd.Flags().GetBool("debug")
+		if err != nil {
+			return err
+		}
+		organizationName, err := cmd.Flags().GetString("name")
+		if err != nil {
+			return err
+		}
+		if organizationName == "" {
+			fmt.Println("Missing arguments: Organization name is not defined")
+			cmd.Help()
+			os.Exit(1)
+		}
+
+		current := lagoonCLIConfig.Current
+		token := lagoonCLIConfig.Lagoons[current].Token
+		lc := lclient.New(
+			lagoonCLIConfig.Lagoons[current].GraphQL,
+			lagoonCLIVersion,
+			&token,
+			debug)
+
+		organizationInput := s.AddOrganizationInput{
+			Name: organizationName,
+		}
+		org := s.Organization{}
+		err = lc.AddOrganization(context.TODO(), &organizationInput, &org)
+		handleError(err)
+
+		resultData := output.Result{
+			Result: "success",
+			ResultData: map[string]interface{}{
+				"Organization Name": organizationName,
+			},
+		}
+		output.RenderResult(resultData, outputOptions)
+		return nil
+	},
+}
+
+func init() {
+	addOrganizationCmd.Flags().String("name", "", "Name of the organization")
+}
