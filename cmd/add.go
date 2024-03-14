905b239054e59ce608da6d9ad831a0ecb707f22d
cgoodwin90 <chris_2190@hotmail.com>
2024-03-14 16:56:04 +1100

    Moved addOrg out of the subcommand

--- cmd/add.go
+++ cmd/add.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"context"
+
 	"github.com/spf13/cobra"
+	"github.com/uselagoon/lagoon-cli/pkg/output"
+	lclient "github.com/uselagoon/machinery/api/lagoon/client"
+	s "github.com/uselagoon/machinery/api/schema"
 )
 
 var addCmd = &cobra.Command{
@@ -25,9 +30,88 @@ var addNotificationCmd = &cobra.Command{
 var addOrganizationCmd = &cobra.Command{
 	Use:     "organization",
 	Aliases: []string{"o"},
-	Short:   "Add an organization, or add a group/project to an organization",
+	Short:   "Add an organization, or add a deploytarget/group/project/user to an organization",
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		validateToken(lagoonCLIConfig.Current) // get a new token if the current one is invalid
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		//fmt.Println(args)
+		//var commandNames []string
+		//for _, command := range cmd.Commands() {
+		//	commandNames = append(commandNames, append(command.Aliases, command.Name())...)
+		//}
+		//fmt.Println(commandNames)
+		//return nil
+		debug, err := cmd.Flags().GetBool("debug")
+		if err != nil {
+			return err
+		}
+		organizationName, err := cmd.Flags().GetString("name")
+		if err != nil {
+			return err
+		}
+		if err := requiredInputCheck("Organization name", organizationName); err != nil {
+			return err
+		}
+		organizationFriendlyName, err := cmd.Flags().GetString("friendly-name")
+		if err != nil {
+			return err
+		}
+		organizationDescription, err := cmd.Flags().GetString("description")
+		if err != nil {
+			return err
+		}
+		organizationQuotaProject, err := cmd.Flags().GetInt("project-quota")
+		if err != nil {
+			return err
+		}
+		organizationQuotaGroup, err := cmd.Flags().GetInt("group-quota")
+		if err != nil {
+			return err
+		}
+		organizationQuotaNotification, err := cmd.Flags().GetInt("notification-quota")
+		if err != nil {
+			return err
+		}
+		organizationQuotaEnvironment, err := cmd.Flags().GetInt("environment-quota")
+		if err != nil {
+			return err
+		}
+		organizationQuotaRoute, err := cmd.Flags().GetInt("route-quota")
+		if err != nil {
+			return err
+		}
+
+		current := lagoonCLIConfig.Current
+		token := lagoonCLIConfig.Lagoons[current].Token
+		lc := lclient.New(
+			lagoonCLIConfig.Lagoons[current].GraphQL,
+			lagoonCLIVersion,
+			&token,
+			debug)
+
+		organizationInput := s.AddOrganizationInput{
+			Name:              organizationName,
+			FriendlyName:      organizationFriendlyName,
+			Description:       organizationDescription,
+			QuotaProject:      organizationQuotaProject,
+			QuotaGroup:        organizationQuotaGroup,
+			QuotaNotification: organizationQuotaNotification,
+			QuotaEnvironment:  organizationQuotaEnvironment,
+			QuotaRoute:        organizationQuotaRoute,
+		}
+		org := s.Organization{}
+		err = lc.AddOrganization(context.TODO(), &organizationInput, &org)
+		handleError(err)
+
+		resultData := output.Result{
+			Result: "success",
+			ResultData: map[string]interface{}{
+				"Organization Name": organizationName,
+			},
+		}
+		output.RenderResult(resultData, outputOptions)
+		return nil
 	},
 }
 
@@ -43,4 +127,13 @@ func init() {
 	addCmd.AddCommand(addUserSSHKeyCmd)
 	addCmd.AddCommand(addVariableCmd)
 	addCmd.AddCommand(addDeployTargetConfigCmd)
+
+	addOrganizationCmd.Flags().StringP("name", "O", "", "Name of the organization")
+	addOrganizationCmd.Flags().String("friendly-name", "", "Friendly name of the organization")
+	addOrganizationCmd.Flags().String("description", "", "Description of the organization")
+	addOrganizationCmd.Flags().Int("project-quota", 0, "Project quota for the organization")
+	addOrganizationCmd.Flags().Int("group-quota", 0, "Group quota for the organization")
+	addOrganizationCmd.Flags().Int("notification-quota", 0, "Notification quota for the organization")
+	addOrganizationCmd.Flags().Int("environment-quota", 0, "Environment quota for the organization")
+	addOrganizationCmd.Flags().Int("route-quota", 0, "Route quota for the organization")
 }
--- cmd/organization.go
+++ cmd/organization.go
@@ -10,87 +10,6 @@ import (
 	s "github.com/uselagoon/machinery/api/schema"
 )
 
-var addOrgCmd = &cobra.Command{
-	Use:     "organization",
-	Aliases: []string{"o"},
-	Short:   "Add a new organization to Lagoon",
-	PreRunE: func(_ *cobra.Command, _ []string) error {
-		return validateTokenE(lagoonCLIConfig.Current)
-	},
-	RunE: func(cmd *cobra.Command, args []string) error {
-		debug, err := cmd.Flags().GetBool("debug")
-		if err != nil {
-			return err
-		}
-		organizationName, err := cmd.Flags().GetString("name")
-		if err != nil {
-			return err
-		}
-		if err := requiredInputCheck("Organization name", organizationName); err != nil {
-			return err
-		}
-		organizationFriendlyName, err := cmd.Flags().GetString("friendly-name")
-		if err != nil {
-			return err
-		}
-		organizationDescription, err := cmd.Flags().GetString("description")
-		if err != nil {
-			return err
-		}
-		organizationQuotaProject, err := cmd.Flags().GetInt("project-quota")
-		if err != nil {
-			return err
-		}
-		organizationQuotaGroup, err := cmd.Flags().GetInt("group-quota")
-		if err != nil {
-			return err
-		}
-		organizationQuotaNotification, err := cmd.Flags().GetInt("notification-quota")
-		if err != nil {
-			return err
-		}
-		organizationQuotaEnvironment, err := cmd.Flags().GetInt("environment-quota")
-		if err != nil {
-			return err
-		}
-		organizationQuotaRoute, err := cmd.Flags().GetInt("route-quota")
-		if err != nil {
-			return err
-		}
-
-		current := lagoonCLIConfig.Current
-		token := lagoonCLIConfig.Lagoons[current].Token
-		lc := lclient.New(
-			lagoonCLIConfig.Lagoons[current].GraphQL,
-			lagoonCLIVersion,
-			&token,
-			debug)
-
-		organizationInput := s.AddOrganizationInput{
-			Name:              organizationName,
-			FriendlyName:      organizationFriendlyName,
-			Description:       organizationDescription,
-			QuotaProject:      organizationQuotaProject,
-			QuotaGroup:        organizationQuotaGroup,
-			QuotaNotification: organizationQuotaNotification,
-			QuotaEnvironment:  organizationQuotaEnvironment,
-			QuotaRoute:        organizationQuotaRoute,
-		}
-		org := s.Organization{}
-		err = lc.AddOrganization(context.TODO(), &organizationInput, &org)
-		handleError(err)
-
-		resultData := output.Result{
-			Result: "success",
-			ResultData: map[string]interface{}{
-				"Organization Name": organizationName,
-			},
-		}
-		output.RenderResult(resultData, outputOptions)
-		return nil
-	},
-}
-
 var deleteOrgCmd = &cobra.Command{
 	Use:     "organization",
 	Aliases: []string{"o"},
@@ -217,7 +136,6 @@ var updateOrganizationCmd = &cobra.Command{
 }
 
 func init() {
-	addOrganizationCmd.AddCommand(addOrgCmd)
 	addOrganizationCmd.AddCommand(addGroupToOrganizationCmd)
 	addOrganizationCmd.AddCommand(addProjectToOrganizationCmd)
 	addOrganizationCmd.AddCommand(addDeployTargetToOrganizationCmd)
@@ -227,15 +145,6 @@ func init() {
 	deleteOrganizationCmd.AddCommand(RemoveDeployTargetFromOrganizationCmd)
 	deleteOrganizationCmd.AddCommand(RemoveProjectFromOrganizationCmd)
 	deleteOrganizationCmd.AddCommand(RemoveUserFromOrganization)
-
-	addOrgCmd.Flags().StringP("name", "O", "", "Name of the organization")
-	addOrgCmd.Flags().String("friendly-name", "", "Friendly name of the organization")
-	addOrgCmd.Flags().String("description", "", "Description of the organization")
-	addOrgCmd.Flags().Int("project-quota", 0, "Project quota for the organization")
-	addOrgCmd.Flags().Int("group-quota", 0, "Group quota for the organization")
-	addOrgCmd.Flags().Int("notification-quota", 0, "Notification quota for the organization")
-	addOrgCmd.Flags().Int("environment-quota", 0, "Environment quota for the organization")
-	addOrgCmd.Flags().Int("route-quota", 0, "Route quota for the organization")
 
 	updateOrganizationCmd.Flags().StringP("name", "O", "", "Name of the organization to update")
 	updateOrganizationCmd.Flags().String("friendly-name", "", "Friendly name of the organization")
