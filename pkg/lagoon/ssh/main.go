279fae0b1bb3e4c5dfc78e8a5f626e178454bc1e
Ben Jackson <shreddedbacon@users.noreply.github.com>
2024-08-14 09:29:15 +1000

    change: ssh host key verification prompts (#370)

--- pkg/lagoon/ssh/main.go
+++ pkg/lagoon/ssh/main.go
@@ -2,11 +2,15 @@
 package ssh
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
+	"net"
 	"os"
+	"path"
 	"strings"
 
+	"github.com/skeema/knownhosts"
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/term"
 )
@@ -140,4 +144,88 @@ func RunSSHCommand(lagoon map[string]string, sshService string, sshContainer str
 	}
 	fmt.Println(b.String())
 	return nil
+}
+
+// make the
+var hostKeyWarn = `The authenticity of host '%s' can't be established.
+%s key fingerprint is %s.
+Are you sure you want to continue connecting %s`
+
+var remoteHostChanged = `@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@    WARNING: REMOTE HOST IDENTIFICATION HAS CHANGED!     @
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+IT IS POSSIBLE THAT SOMEONE IS DOING SOMETHING NASTY!
+Someone could be eavesdropping on you right now (man-in-the-middle attack)!
+It is also possible that a host key has just been changed.
+The fingerprint for the %s key sent by the remote host is
+%s
+Add correct host key in %s to get rid of this message`
+
+// add interactive known hosts to reduce confusion with host key errors
+func InteractiveKnownHosts(userPath, host string, ignorehost, accept bool) (ssh.HostKeyCallback, []string, error) {
+	kh, err := knownhosts.NewDB(path.Join(userPath, ".ssh/known_hosts"))
+	if err != nil {
+		return nil, nil, fmt.Errorf("couldn't get ~/.ssh/known_hosts: %v", err)
+	}
+	if ignorehost {
+		// if ignore provided, just skip the hostkey verifications
+		return ssh.InsecureIgnoreHostKey(), kh.HostKeyAlgorithms(host), nil
+	}
+	// otherwise prompt or accept for the key if required
+	return ssh.HostKeyCallback(func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+		filePath := path.Join(userPath, ".ssh/known_hosts")
+		innerCallback := kh.HostKeyCallback()
+		err := innerCallback(hostname, remote, key)
+		sshPubKey := ssh.MarshalAuthorizedKey(key)
+		pub, _, _, _, perr := ssh.ParseAuthorizedKey(sshPubKey)
+		if perr != nil {
+			return fmt.Errorf("knownhosts: host key verification failed")
+		}
+		if knownhosts.IsHostKeyChanged(err) {
+			os.Stderr.WriteString(fmt.Sprintf(remoteHostChanged, key.Type(), ssh.FingerprintSHA256(pub), filePath))
+			return fmt.Errorf("knownhosts: host key verification failed")
+		} else if knownhosts.IsHostUnknown(err) {
+			f, ferr := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 0600)
+			if ferr == nil {
+				defer f.Close()
+				response := "n"
+				if accept {
+					response = "y"
+				} else {
+					os.Stderr.WriteString(fmt.Sprintf(hostKeyWarn, hostname, key.Type(), ssh.FingerprintSHA256(pub), "(yes/no)? "))
+					reader := bufio.NewReader(os.Stdin)
+					response, err = reader.ReadString('\n')
+					if err != nil {
+						return fmt.Errorf("knownhosts: host key verification failed %v", err)
+					}
+					response = strings.ToLower(strings.TrimSpace(response))
+				}
+				if response == "Yes" || response == "yes" || response == "y" {
+					ferr = knownhosts.WriteKnownHost(f, hostname, remote, key)
+				} else {
+					return fmt.Errorf("knownhosts: host key verification failed")
+				}
+			}
+			if ferr == nil {
+				os.Stderr.WriteString(fmt.Sprintf("Warning: Permanently added '%s' to the list of known hosts\n", hostname))
+			} else {
+				os.Stderr.WriteString(fmt.Sprintf("Failed to add host %s to known_hosts: %v\n", hostname, ferr))
+			}
+			return nil // permit previously-unknown hosts (warning: may be insecure)
+		}
+		return err
+	}), kh.HostKeyAlgorithms(host), nil
+}
+
+func CheckStrictHostKey(v string) (bool, bool) {
+	if v == "ignore" || v == "no" {
+		// return ignore true, accept false
+		return true, false
+	}
+	if v == "accept-new" {
+		// return ignore false, accept true
+		return false, true
+	}
+	// default "accept-new"
+	return false, false
 }
