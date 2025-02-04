1f728ed9d7f1bf78b035b0fcb8d418b78247983c
shreddedbacon <b@benjackson.email>
2025-02-04 17:50:58 +1100

    chore: remove import export commands

--- cmd/config_test.go
+++ cmd/config_test.go
@@ -5,7 +5,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/uselagoon/lagoon-cli/internal/lagoon"
+	"github.com/uselagoon/lagoon-cli/internal/config"
 )
 
 func TestConfigRead(t *testing.T) {
@@ -13,16 +13,16 @@ func TestConfigRead(t *testing.T) {
 		input           string
 		description     string
 		readfailallowed bool
-		expect          lagoon.Config
+		expect          config.Config
 	}{
 		"valid-yaml": {
 			input:           "testdata/lagoon.yml",
 			description:     "This test checks that a valid and complete configuration is parsed",
 			readfailallowed: false,
-			expect: lagoon.Config{
+			expect: config.Config{
 				Current: "amazeeio",
 				Default: "amazeeio",
-				Lagoons: map[string]lagoon.Context{
+				Lagoons: map[string]config.Context{
 					"amazeeio": {
 						GraphQL:  "https://api.amazeeio.cloud/graphql",
 						HostName: "token.amazeeio.cloud",
@@ -39,16 +39,16 @@ func TestConfigRead(t *testing.T) {
 			input:           "testdata/lagoon.yml.invalid",
 			description:     "This test checks to see if an invalid yaml config is not parsed",
 			readfailallowed: true,
-			expect:          lagoon.Config{},
+			expect:          config.Config{},
 		},
 		"missing-yaml": {
 			input:           "testdata/lagoon.yml.missing",
 			description:     "This test checks if a context is missing the required data (graphql, hostname, port)",
 			readfailallowed: true,
-			expect: lagoon.Config{
+			expect: config.Config{
 				Current: "amazeeio",
 				Default: "amazeeio",
-				Lagoons: map[string]lagoon.Context{
+				Lagoons: map[string]config.Context{
 					"amazeeio": {
 						Kibana: "https://logs.amazeeio.cloud/",
 						UI:     "https://dashboard.amazeeio.cloud",
@@ -61,7 +61,7 @@ func TestConfigRead(t *testing.T) {
 	}
 	for name, tc := range testCases {
 		t.Run(name, func(tt *testing.T) {
-			lc := lagoon.Config{}
+			lc := config.Config{}
 			if err := readLagoonConfig(&lc, tc.input); err != nil {
 				if tc.readfailallowed == false {
 					tt.Fatal(err)
